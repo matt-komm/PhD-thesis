3f25b89ad2d404c46a3e819c7c4d559ba602e961
matt-komm <mkomm@cern.ch>
2016-11-15 21:27:38 +0100

    fixes

--- compile.sh
+++ compile.sh
@@ -3,22 +3,16 @@ pdflatex \
 -output-directory temp \
 -file-line-error \
 -halt-on-error \
-main.tex
-
-
-
-bibtex temp/main.aux
-
-cd temp
-makeglossaries main
-cd ..
-
+main.tex && \
+bibtex temp/main.aux && \
+cd temp && \
+makeglossaries main && \
+cd .. && \
 pdflatex \
 -output-directory temp \
 -file-line-error \
 -halt-on-error \
-main.tex
-
+main.tex && \
 pdflatex \
 -output-directory temp \
 -file-line-error \
