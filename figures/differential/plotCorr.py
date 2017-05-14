90b0af5e5c0ba450f0c6f64d4e80912f1ca4912d
matt-komm <mkomm@cern.ch>
2017-05-14 23:35:41 +0200

    up

---
+++ figures/differential/plotCorr.py
@@ -0,0 +1,319 @@
+#!/usr/bin/python
+
+import ROOT
+import numpy
+import random
+import math
+import os
+import re
+from optparse import OptionParser
+
+cvscale = 1.0
+
+fontScale = 600./650.
+
+ROOT.gROOT.Reset()
+ROOT.gROOT.SetBatch(True)
+ROOT.gStyle.SetOptStat(0)
+ROOT.gStyle.SetOptFit(0)
+ROOT.gROOT.Reset()
+ROOT.gROOT.SetStyle("Plain")
+ROOT.gStyle.SetOptStat(0)
+ROOT.gStyle.SetOptFit(1111)
+ROOT.gStyle.SetPadTopMargin(0.08)
+ROOT.gStyle.SetPadLeftMargin(0.13)
+ROOT.gStyle.SetPadRightMargin(0.26)
+ROOT.gStyle.SetPadBottomMargin(0.15)
+ROOT.gStyle.SetStatFontSize(0.025)
+
+ROOT.gStyle.SetOptFit()
+ROOT.gStyle.SetOptStat(0)
+
+# For the canvas:
+ROOT.gStyle.SetCanvasBorderMode(0)
+ROOT.gStyle.SetCanvasColor(ROOT.kWhite)
+ROOT.gStyle.SetCanvasDefH(600) #Height of canvas
+ROOT.gStyle.SetCanvasDefW(600) #Width of canvas
+ROOT.gStyle.SetCanvasDefX(0)   #POsition on screen
+ROOT.gStyle.SetCanvasDefY(0)
+
+# For the Pad:
+ROOT.gStyle.SetPadBorderMode(0)
+# ROOT.gStyle.SetPadBorderSize(Width_t size = 1)
+ROOT.gStyle.SetPadColor(ROOT.kWhite)
+#ROOT.gStyle.SetPadGridX(True)
+#ROOT.gStyle.SetPadGridY(True)
+ROOT.gStyle.SetGridColor(ROOT.kBlack)
+ROOT.gStyle.SetGridStyle(2)
+ROOT.gStyle.SetGridWidth(1)
+
+# For the frame:
+
+ROOT.gStyle.SetFrameBorderMode(0)
+ROOT.gStyle.SetFrameBorderSize(0)
+ROOT.gStyle.SetFrameFillColor(0)
+ROOT.gStyle.SetFrameFillStyle(0)
+ROOT.gStyle.SetFrameLineColor(1)
+ROOT.gStyle.SetFrameLineStyle(1)
+ROOT.gStyle.SetFrameLineWidth(0)
+
+# For the histo:
+# ROOT.gStyle.SetHistFillColor(1)
+# ROOT.gStyle.SetHistFillStyle(0)
+# ROOT.gStyle.SetLegoInnerR(Float_t rad = 0.5)
+# ROOT.gStyle.SetNumberContours(Int_t number = 20)
+
+ROOT.gStyle.SetEndErrorSize(2)
+#ROOT.gStyle.SetErrorMarker(20)
+ROOT.gStyle.SetErrorX(0.)
+
+ROOT.gStyle.SetMarkerStyle(20)
+#ROOT.gStyle.SetMarkerStyle(20)
+
+#For the fit/function:
+ROOT.gStyle.SetOptFit(1)
+ROOT.gStyle.SetFitFormat("5.4g")
+ROOT.gStyle.SetFuncColor(2)
+ROOT.gStyle.SetFuncStyle(1)
+ROOT.gStyle.SetFuncWidth(1)
+
+#For the date:
+ROOT.gStyle.SetOptDate(0)
+# ROOT.gStyle.SetDateX(Float_t x = 0.01)
+# ROOT.gStyle.SetDateY(Float_t y = 0.01)
+
+# For the statistics box:
+ROOT.gStyle.SetOptFile(0)
+ROOT.gStyle.SetOptStat(0) # To display the mean and RMS:   SetOptStat("mr")
+ROOT.gStyle.SetStatColor(ROOT.kWhite)
+ROOT.gStyle.SetStatFont(42)
+ROOT.gStyle.SetStatFontSize(0.025)
+ROOT.gStyle.SetStatTextColor(1)
+ROOT.gStyle.SetStatFormat("6.4g")
+ROOT.gStyle.SetStatBorderSize(1)
+ROOT.gStyle.SetStatH(0.1)
+ROOT.gStyle.SetStatW(0.15)
+
+ROOT.gStyle.SetHatchesSpacing(0.8/math.sqrt(cvscale))
+ROOT.gStyle.SetHatchesLineWidth(int(2*cvscale))
+
+# ROOT.gStyle.SetStaROOT.TStyle(Style_t style = 1001)
+# ROOT.gStyle.SetStatX(Float_t x = 0)
+# ROOT.gStyle.SetStatY(Float_t y = 0)
+
+
+#ROOT.gROOT.ForceStyle(True)
+#end modified
+
+# For the Global title:
+
+ROOT.gStyle.SetOptTitle(0)
+
+# ROOT.gStyle.SetTitleH(0) # Set the height of the title box
+# ROOT.gStyle.SetTitleW(0) # Set the width of the title box
+#ROOT.gStyle.SetTitleX(0.35) # Set the position of the title box
+#ROOT.gStyle.SetTitleY(0.986) # Set the position of the title box
+# ROOT.gStyle.SetTitleStyle(Style_t style = 1001)
+#ROOT.gStyle.SetTitleBorderSize(0)
+
+# For the axis titles:
+ROOT.gStyle.SetTitleColor(1, "XYZ")
+ROOT.gStyle.SetTitleFont(43, "XYZ")
+ROOT.gStyle.SetTitleSize(35*fontScale*cvscale, "XYZ")
+# ROOT.gStyle.SetTitleXSize(Float_t size = 0.02) # Another way to set the size?
+# ROOT.gStyle.SetTitleYSize(Float_t size = 0.02)
+ROOT.gStyle.SetTitleXOffset(1.5)
+#ROOT.gStyle.SetTitleYOffset(1.2)
+ROOT.gStyle.SetTitleOffset(1.2, "YZ") # Another way to set the Offset
+
+# For the axis labels:
+
+ROOT.gStyle.SetLabelColor(1, "XYZ")
+ROOT.gStyle.SetLabelFont(43, "XYZ")
+ROOT.gStyle.SetLabelOffset(0.0077, "XYZ")
+ROOT.gStyle.SetLabelOffset(0.011, "X")
+ROOT.gStyle.SetLabelSize(32*fontScale*cvscale, "XYZ")
+#ROOT.gStyle.SetLabelSize(0.04, "XYZ")
+
+# For the axis:
+
+ROOT.gStyle.SetAxisColor(1, "XYZ")
+ROOT.gStyle.SetAxisColor(1, "XYZ")
+ROOT.gStyle.SetStripDecimals(True)
+ROOT.gStyle.SetTickLength(0.03, "Y")
+ROOT.gStyle.SetTickLength(0.05, "X")
+ROOT.gStyle.SetNdivisions(1005, "X")
+ROOT.gStyle.SetNdivisions(506, "Y")
+
+ROOT.gStyle.SetPadTickX(1)  # To get tick marks on the opposite side of the frame
+ROOT.gStyle.SetPadTickY(1)
+
+# Change for log plots:
+ROOT.gStyle.SetOptLogx(0)
+ROOT.gStyle.SetOptLogy(0)
+ROOT.gStyle.SetOptLogz(0)
+
+#ROOT.gStyle.SetPalette(1) #(1,0)
+
+# another top group addition
+
+# Postscript options:
+#ROOT.gStyle.SetPaperSize(20., 20.)
+#ROOT.gStyle.SetPaperSize(ROOT.TStyle.kA4)
+#ROOT.gStyle.SetPaperSize(27., 29.7)
+#ROOT.gStyle.SetPaperSize(27., 29.7)
+ROOT.gStyle.SetPaperSize(8.0*1.6*cvscale,6.0*1.6*cvscale)
+ROOT.TGaxis.SetMaxDigits(3)
+ROOT.gStyle.SetLineScalePS(2)
+
+# ROOT.gStyle.SetLineStyleString(Int_t i, const char* text)
+# ROOT.gStyle.SetHeaderPS(const char* header)
+# ROOT.gStyle.SetTitlePS(const char* pstitle)
+#ROOT.gStyle.SetColorModelPS(1)
+
+# ROOT.gStyle.SetBarOffset(Float_t baroff = 0.5)
+# ROOT.gStyle.SetBarWidth(Float_t barwidth = 0.5)
+# ROOT.gStyle.SetPaintTextFormat(const char* format = "g")
+# ROOT.gStyle.SetPalette(Int_t ncolors = 0, Int_t* colors = 0)
+# ROOT.gStyle.SetTimeOffset(Double_t toffset)
+# ROOT.gStyle.SetHistMinimumZero(kTRUE)
+ROOT.gStyle.SetPalette(1)
+ROOT.gStyle.SetPaintTextFormat("3.0f")
+
+stops = numpy.array([0.00,0.15, 0.34, 0.45, 0.65, 0.84, 1.00])
+red   = numpy.array([0.50,0.05, 0.05, 0.4, 1.0, 1.00, 0.95])
+green = numpy.array([0.00,0.05, 0.8, 0.9, 0.8,0.20, 0.05])
+blue  = numpy.array([1.00,0.87, 1.00, 0., 0.0,0.00, 0.05])
+
+for i in range(len(red)):
+    red[i]=min(red[i]*1.1,1.0)
+    green[i]=min(green[i]*1.05,1.0)
+    blue[i]=min(blue[i]*1.1+0.1,1.0)
+
+start=ROOT.TColor.CreateGradientColorTable(len(stops), stops, red, green, blue, 200)
+ROOT.gStyle.SetNumberContours(200)
+
+
+
+colors=[]
+def newColor(red,green,blue):
+    newColor.colorindex+=1
+    color=ROOT.TColor(newColor.colorindex,red,green,blue)
+    colors.append(color)
+    return color
+    
+newColor.colorindex=301
+
+def getDarkerColor(color):
+    darkerColor=newColor(color.GetRed()*0.6,color.GetGreen()*0.6,color.GetBlue()*0.6)
+    return darkerColor
+
+
+
+            
+cv = ROOT.TCanvas("cv"+str(random.random()),"",int(cvscale*1150),int(cvscale*700))
+cv.SetPad(0.0, 0.0, 1.0, 1.0)
+cv.SetFillStyle(4000)
+
+cvxmin=0.23
+cvxmax=0.82
+cvymin=0.19
+cvymax=0.92
+            
+            
+cv.SetBorderMode(0)
+cv.SetGridx(False)
+cv.SetGridy(False)
+
+
+#For the frame:
+cv.SetFrameBorderMode(0)
+cv.SetFrameBorderSize(1)
+cv.SetFrameFillColor(0)
+cv.SetFrameFillStyle(0)
+cv.SetFrameLineColor(1)
+cv.SetFrameLineStyle(1)
+cv.SetFrameLineWidth(int(1*cvscale))
+
+# Margins:
+cv.SetLeftMargin(cvxmin)
+cv.SetRightMargin(1-cvxmax)
+
+# For the Global title:
+cv.SetTitle("")
+
+# For the axis:
+cv.SetTickx(1)  # To get tick marks on the opposite side of the frame
+cv.SetTicky(1)
+
+# Change for log plots:
+cv.SetLogx(0)
+cv.SetLogy(0)
+cv.SetLogz(0)
+                    
+cv.SetTopMargin(1-cvymax)
+cv.SetBottomMargin(cvymin)
+
+
+corr = [
+    [0.085,-0.02,-0.063,-0.025,0.014,-0.0078,1],
+    [-0.057,0.032,-0.017,0.013,-0.14,1],
+    [0.011,-0.058,-0.03,0.56,1],
+    [0.062,-0.013,0.16,1],
+    [-0.035,0.28,1],
+    [-0.11,1],
+    [1]
+]
+
+labels = [
+"top quark |y|",
+"top quark p#lower[0.3]{#scale[0.7]{T}}",
+"  j' |#eta|",
+"#Delta R(j',#kern[-0.5]{ }b jet)",
+"|#Delta #eta (b jet,#kern[-0.5]{ }muon)|",
+"top quark mass",
+"m#lower[0.3]{#scale[0.7]{T}}(W)"
+]
+
+hist = ROOT.TH2F("corr","",len(corr[0]),0,len(corr[0]),len(corr[0]),0,len(corr[0]))
+hist.GetZaxis().Set(0,-100,100)
+hist.GetZaxis().SetRangeUser(-100,100)
+hist.GetZaxis().SetTitle("Correlation (%)")
+hist.SetMarkerSize(1.5)
+for ibin,corrX in enumerate(reversed(corr)):
+    for jbin,val in enumerate(corrX):
+        hist.SetBinContent(ibin+1,jbin+1,val*100.)
+        hist.SetBinContent(jbin+1,ibin+1,val*100.)
+hist.Draw("colztext")
+
+for i,text in enumerate(reversed(labels)):
+    hist.GetXaxis().SetBinLabel(i+1,text)
+    hist.GetYaxis().SetBinLabel(i+1,text)
+    
+    
+pCMS=ROOT.TPaveText(cvxmin+0.025,0.94,cvxmin+0.025,0.94,"NDC")
+pCMS.SetFillColor(ROOT.kWhite)
+pCMS.SetBorderSize(0)
+pCMS.SetTextFont(63)
+pCMS.SetTextSize(34*cvscale)
+pCMS.SetTextAlign(11)
+pCMS.AddText("CMS")
+pCMS.Draw("Same")
+
+pPreliminary=ROOT.TPaveText(cvxmin+0.025+0.08,0.94,cvxmin+0.025+0.08,0.94,"NDC")
+pPreliminary.SetFillColor(ROOT.kWhite)
+pPreliminary.SetBorderSize(0)
+pPreliminary.SetTextFont(53)
+pPreliminary.SetTextSize(34*cvscale)
+pPreliminary.SetTextAlign(11)
+pPreliminary.AddText("Preliminary")
+pPreliminary.Draw("Same")
+                
+                
+cv.Update()
+#cv.WaitPrimitive()
+cv.Print("ObsCorrelations.pdf")
+
+
+
+
